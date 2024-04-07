166620f153bee9158213e691c776f33249b672b0
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-04-07 01:22:15 +0000

    Login Update

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ import services.connect as C
 
 st.title("Bem vindo ao app de recolhimentos do Magazine Luiza CD 2650.")
 
-User = st.text_input('Usuário')
+with st.form('LoginForm')
+    User = st.number_input('Filial',step=1)
 
-Pass = st.text_input("Senha")
+    Pass = st.text_input("Senha")
 
-login_button = st.button("Login")
+    login_button = st.form_submit_button("Login")
 
-if login_button:
-    if Pass == '123':
-        st.session_state['Usuário'] = User
-        st.session_state['Login'] = 'Logado'
-        st.warning('Login realizado com sucesso!')
-        sleep(2)
-        st.switch_page('./pages/Solicitação.py')
-        
-    else:
-        st.error("Favor verificar os dados.")
+    if login_button:
+        if Pass == '123':
+            st.session_state['Usuário'] = User
+            st.session_state['Login'] = 'Logado'
+            st.warning('Login realizado com sucesso!')
+            sleep(2)
+            st.switch_page('./pages/Solicitação.py')
+            
+        else:
+            st.error("Favor verificar os dados.")
         
 
