8da2d5ec67fa0b23f5f0e613c454684ea3d3c313
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-05-25 16:44:52 -0300

    Update connect.py

--- services/connect.py
+++ services/connect.py
@@ -4,6 +4,7 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from datetime import timedelta, datetime
 import pandas as pd
+import streamlit as st
 
 
 json = {
