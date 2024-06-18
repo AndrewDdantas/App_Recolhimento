da24d5e95547254b8f9a005d0b46af9c6bbf4839
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-06-18 10:32:44 -0300

    Create RegistrarLen.py

---
+++ pages/RegistrarLen.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+import services.connect as C
+from time import sleep
+
+if 'df' not in st.session_state:
+    data = {'Filial': [],
+        'Pedido': [],
+        'Transportadora': [],
+        'Nota':[]}
+    st.session_state['df'] = pd.DataFrame(data)
+
+elif not st.session_state['df'].empty:
+    st.dataframe(st.session_state['df'], hide_index=True)
+    button_registrar = st.button("Gerar Lote")
+
+try:
+    if button_registrar:
+        with st.spinner('Lote sendo gerado......'):
+            lote = C.registrar_pedidos(st.session_state['df'])
+            st.session_state['df'] = pd.DataFrame()
+        st.warning(f'Foi gerado o Lote: {lote}')
+        st.rerun()
+except:
+    ''
+
+with st.form('Solicitar'):
+    filial = st.number_input('Filial', step=1)
+    pedido = st.number_input('Pedido', step=1, min_value=900000000)
+    transportadora = st.selectbox('Transportadora', C.transportadoras())
+    nota = st.file_uploader('Nota Fiscal')
+
+    submit = st.form_submit_button('Inserir em Lote')
+
+    if submit:
+        nota = C.upload_arquivo(nota,pedido)
+        new_row = {'Filial': filial,
+        'Pedido': pedido,
+        'Transportadora': transportadora,
+        'Nota': nota}
+        new_df = pd.DataFrame([new_row])
+        st.session_state['df'] = pd.concat([st.session_state['df'], new_df], ignore_index=True)
+        st.rerun()
