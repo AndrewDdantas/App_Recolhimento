a6500c3288c3ab1577acd674d642443dc365ead7
AndrewDdantas <151173924+AndrewDdantas@users.noreply.github.com>
2024-06-25 13:47:27 -0300

    Update connect.py

--- services/connect.py
+++ services/connect.py
@@ -103,7 +103,7 @@ def registrar_pedidos(df):
         base_len.update(df.values.tolist(), 'a'+str(last_row),value_input_option='USER_ENTERED')
         return last_lot
     except:
-        return 'Deu Merda'
+        return 'Deu Erro'
 
 def consultar_pedidos_saida_loja(pedido=None, filial=0):
     df = pd.DataFrame(base_len.get_values('a2:h'))
